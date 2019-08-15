f2fa1c4eeae0df5cea5091a8968532551313024e
Frances Cancio <frances.cancio@gmail.com>
2019-08-15 16:59:08 -0500

    Redirect added new flight to flights page

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,7 +3,17 @@ var Flight = require('../models/flight');
 module.exports = {
     index,
     new: newFlight,
+    create,
 };
+
+function create(req, res) {
+    var flight = new Flight(req.body);
+    flight.save(function(err) {
+        if (err) return res.redirect('/flights/new');
+        console.log(flight);
+        res.redirect('/flights');
+    });
+}
 
 function newFlight(req, res) {
     res.render('flights/new', { title: 'Add Flight' });
