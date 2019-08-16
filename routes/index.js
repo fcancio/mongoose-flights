09491b6acc2ccea6d234c60056436f12737a2153
Frances Cancio <frances.cancio@gmail.com>
2019-08-15 22:32:13 -0500

    Adds styling to homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mongoose Flights' });
+  res.render('index', { title: 'Hello Kitty Flights' });
 });
 
 module.exports = router;
