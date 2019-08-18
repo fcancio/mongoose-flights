6f4f13e7a7fd381e4114d43fcbd89dff0331bbc5
Frances Cancio <frances.cancio@gmail.com>
2019-08-18 17:54:35 -0500

    Adds functionality for new and create ticket, and lists on show view

---
+++ controllers/tickets.js
@@ -0,0 +1,24 @@
+var Ticket = require('../models/ticket');;
+var Flight = require('../models/flight')
+
+module.exports = {
+  new: newTicket,
+  create,
+};
+
+function create(req, res) {
+    req.body.flight = req.params.id;
+    Ticket.create(req.body, function(err, ticket) {
+        res.redirect(`/flights/${req.params.id}`);
+  });
+}
+
+
+function newTicket(req, res) {
+  Flight.findById(req.params.id, function(err, flight) {
+    res.render('tickets/new', {
+      title: 'Add Ticket',
+      flight
+  });
+});
+}
